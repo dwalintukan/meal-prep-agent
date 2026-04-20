5c917ade6def61a99d4f1c117f20b5b181b6f086
Deric Walintukan <deric.walintukan@gmail.com>
2026-04-19 19:28:05 -0700

    add WeeklyPlan model

--- models/domain.py
+++ models/domain.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, date
 
 
 class Ingredient(BaseModel):
@@ -18,4 +18,10 @@ class Recipe(BaseModel):
     prep_minutes: int
     cook_minutes: int
     tags: list[str]
+    created_at: datetime
+
+
+class WeeklyPlan(BaseModel):
+    timestamp: date
+    recipe_ids: list[int]
     created_at: datetime
