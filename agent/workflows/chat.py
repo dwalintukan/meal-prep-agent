0662543a270569e0ec9a9cc503dbaaa53d79a7a4
Deric Walintukan <deric.walintukan@gmail.com>
2026-05-07 21:04:05 -0700

    add chat workflow

---
+++ agent/workflows/chat.py
@@ -0,0 +1,9 @@
+class ChatWorkflow:
+    def __init__(self):
+        pass
+
+    def run(self) -> str:
+        return (
+            "I can help you plan meals and add recipes. "
+            "Try saying: 'Plan my meals', 'What should I eat this week', 'Make me a meal plan'"
+        )
