41bb638fca6880d41e6d0e7d71e69a1b33b03676
Deric Walintukan <deric.walintukan@gmail.com>
2026-07-05 14:54:52 +0900

    implement google login api

--- api/auth.py
+++ api/auth.py
@@ -1,11 +1,30 @@
-from fastapi import APIRouter
+import os
+import secrets
+from urllib.parse import urlencode
+from fastapi import APIRouter, Request
+from fastapi.routing import RedirectResponse
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
+
 
 @router.get("/google")
-async def google_login():
-    pass
+async def google_login(request: Request):
+    state = secrets.token_urlsafe(16)
+    request.session["oauth_state"] = state
+
+    params = {
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "response_type": "code",
+        "scope": "openid email profile",
+        "state": state,
+    }
+    url = f"https://accounts.google.com/o/oauth2/v2/auth?{urlencode(params)}"
+    return RedirectResponse(url)
 
 
 @router.get("/google/callback")
